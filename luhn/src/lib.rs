debb9000b34502ad207f2cc983c8769d4984a52f
Chris Wilding <hello@chriswilding.co.uk>
2021-12-04 22:13:24 +0000

    Update luhn

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -1,22 +1,13 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    if !code.chars().all(|c| c.is_numeric() || c.is_whitespace()) {
-        return false;
-    }
-
-    let cleaned: Vec<_> = code.chars().filter(|c| !c.is_whitespace()).rev().collect();
-
-    if cleaned.len() == 1 && cleaned[0] == '0' {
-        return false;
-    }
-
-    let sum: u32 = cleaned
-        .iter()
-        .map(|c| c.to_digit(10).unwrap())
-        .zip(0..16)
-        .map(|(d, i)| if i % 2 == 0 { d } else { d * 2 })
-        .map(|d| if d > 9 { d - 9 } else { d })
-        .sum();
-
-    sum % 10 == 0
+    code.chars()
+        .rev()
+        .filter(|c| !c.is_whitespace())
+        .try_fold((0, 0), |(sum, count), c| {
+            c.to_digit(10)
+                .map(|d| if count % 2 == 0 { d } else { d * 2 })
+                .map(|d| if d > 9 { d - 9 } else { d })
+                .map(|d| (sum + d, count + 1))
+        })
+        .map_or(false, |(sum, count)| count > 1 && sum % 10 == 0)
 }
