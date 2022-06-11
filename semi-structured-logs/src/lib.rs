dbffbe5f657c616e2a366766a754c28e1a9b6057
Chris Wilding <hello@chriswilding.co.uk>
2022-06-11 17:31:33 +0100

    Add semi-structured-logs

---
+++ semi-structured-logs/src/lib.rs
@@ -0,0 +1,36 @@
+use std::fmt;
+
+/// various log levels
+#[derive(Clone, PartialEq, Debug)]
+pub enum LogLevel {
+    Info,
+    Warning,
+    Error,
+}
+
+impl fmt::Display for LogLevel {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Info => write!(f, "INFO"),
+            Self::Warning => write!(f, "WARNING"),
+            Self::Error => write!(f, "ERROR"),
+        }
+    }
+}
+
+pub fn log(level: LogLevel, message: &str) -> String {
+    let level = format!("{:?}", level);
+    format!("[{}]: {}", level.to_uppercase(), message)
+}
+
+pub fn info(message: &str) -> String {
+    log(LogLevel::Info, message)
+}
+
+pub fn warn(message: &str) -> String {
+    log(LogLevel::Warning, message)
+}
+
+pub fn error(message: &str) -> String {
+    log(LogLevel::Error, message)
+}
