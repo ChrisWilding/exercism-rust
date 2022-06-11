5f2c116c4f401090666b6b714e39a01bd52ba92c
Chris Wilding <hello@chriswilding.co.uk>
2022-06-11 22:16:32 +0100

    Update gigasecond

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,6 +1,6 @@
 use time::PrimitiveDateTime as DateTime;
 
-const GIGASECOND: time::Duration = time::Duration::new(1_000_000_000, 0);
+const GIGASECOND: time::Duration = time::Duration::seconds(1_000_000_000);
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
